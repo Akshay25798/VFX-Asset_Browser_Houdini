1c0055442bd9ab9904a95569c896e0c92eb96373
Akshay Kumar <akshayabhikumar25798@gmail.com>
2024-05-01 04:14:43 +0530

    modified:   python_panels/akAssetBrowser.pypanel 	modified:   src/__pycache__/main.cpython-37.pyc 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -4,10 +4,9 @@ from PySide2 import QtWidgets, QtGui, QtCore
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import Qt
 import json, requests, os
-
-import layout
 from .worker import Worker
 from .flowLayout import FlowLayout
+from multiprocessing import Process
 
 ##
 #path construntion
@@ -44,12 +43,31 @@ def get_houdini_icon(icon, size=50):
 
 ##--class starts
 
+class SetLocalIcon(QtCore.QObject):
+    def __init__(self, parent, req):
+        self.req = req
+        self.pixmap = QtGui.QPixmap()
+        super(SetLocalIcon, self).__init__(parent)
+
+    def start_fetch(self, net_mgr):
+        self.fetch_task = net_mgr.get(self.req)
+        self.fetch_task.finished.connect(self.resolve_fetch)
+
+    def resolve_fetch(self):
+        local_file_path = thumbnail_folder + self.parent().objectName() + ".png"
+        with open(local_file_path, mode="rb") as local_thumbnail_file:
+            the_reply = local_thumbnail_file.read()
+            self.set_widget_image(the_reply)
+
+    def set_widget_image(self, img_binary):
+            self.pixmap.loadFromData(img_binary)
+            self.parent().setIcon(self.pixmap)
+                       
 class IconDownloader(QtCore.QObject):
     def __init__(self, parent, req):
         self.req = req
         self.pixmap = QtGui.QPixmap()
         super(IconDownloader, self).__init__(parent)
-
 
     def start_fetch(self, net_mgr):
         self.fetch_task = net_mgr.get(self.req)
@@ -69,16 +87,11 @@ class IconDownloader(QtCore.QObject):
         icon.addPixmap(local_file_path)
         self.parent().setIcon(icon)
 
-        # self.pixmap.loadFromData(img_binary) #load directly form web/api
-        # icon = QtGui.QIcon()
-        # icon.addPixmap(self.pixmap)
-        # self.parent().setIcon(icon)
-
-
 
 class MainAssetBrowserUI(QtWidgets.QWidget):
     def __init__(self):
         super(MainAssetBrowserUI, self).__init__()
+        print("Asset_Brower_Started")
         #managers 
         self.download_queue = QtNetwork.QNetworkAccessManager()
         self.threadpool = QtCore.QThreadPool.globalInstance()
@@ -116,54 +129,119 @@ class MainAssetBrowserUI(QtWidgets.QWidget):
 
         #set property
         self.progress_bar.setProperty("visible", False)
-        self.get_asset_type()
+
+        self.set_cagagories()
         # self.set_icons()
-        self.asset_type.currentIndexChanged.connect(self.get_asset_type)
+        self.asset_type.currentIndexChanged.connect(self.set_cagagories)
+        self.asset_type.currentIndexChanged.connect(self.set_icons)
+        
+        self.asset_catagories.currentIndexChanged.connect(self.set_icons)
+
         self.icon_size_slider.valueChanged.connect(self.set_icons_size)
         self.tex_res.currentIndexChanged.connect(self.check_asset_download_status)
         self.asset_format.currentIndexChanged.connect(self.check_asset_download_status)
-        self.asset_catagories.currentIndexChanged.connect(self.get_asset_type)
-
         
+
+        self.setLayout(self.main_layout)
 
 
 
 
     ##--functions starts--
+        
 
-    def get_asset_type(self):
-        if len(os.listdir(json_folder))==0:
+    def set_icons(self, size=200):
+
+        if self.asset_type.currentIndex() == 0:
+            json_file = json_folder + "hdris.json"
+        elif self.asset_type.currentIndex() == 1:
+            json_file = json_folder + "models.json"
+        elif self.asset_type.currentIndex() == 2:
+            json_file = json_folder + "textures.json"
+        else:
+            json_file = "hdris"
+
+        self.clear_layout(self.assets_view) #clear the content area for new asset
+
+        asset_in_catagory = [] #assets in current catagory
+        current_catagory = self.asset_catagories.currentText()
+
+        with open(json_file, "r") as read_content: #check if asset in current catagory
+            data = json.load(read_content)
+            for key in data.keys():
+                if current_catagory in (data[key]["categories"]):
+                    asset_in_catagory.append(key) #add asset to asset_in_catagory list
+
+        with open(json_file, "r") as read_content: #ping the local json
+            data = json.load(read_content)
+
+        for key in asset_in_catagory: #get and set thumbnails for assets in catagory
+            btn = QtWidgets.QToolButton()
+            btn.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+            btn.setFixedSize(QtCore.QSize(size, size))
+            btn.setIconSize(QtCore.QSize(size, size))
+            btn.setText(key.replace("_", " ").title())
+            btn.setObjectName(key)
+            btn.setStyleSheet("QToolButton{font-family: Roboto}")
+            #icon add to content area
+            self.assets_view.setSpacing(4)
+            self.assets_view.addWidget(btn)
+
+            thumb_name = ("{0}.png".format(btn.objectName()))
+            if not thumb_name in os.listdir(thumbnail_folder): #get icons form api
+                self.status_bar.setText("downloading %s thumbnail"%(thumb_name))
+                url = thumb_url + key + ".png?height=" + str(500)
+                req = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
+                
+                download = IconDownloader(btn, req)
+                download.start_fetch(self.download_queue)
+
+            #get icons form local thumbnail folder
+            get_local_thumb = thumbnail_folder + thumb_name
+            
+            local_icon = SetLocalIcon(btn, QtNetwork.QNetworkRequest(QtCore.QUrl(get_local_thumb)))
+            local_icon.start_fetch(self.download_queue)
+            
+            # self.set_local_icon(btn, get_local_thumb)
+
+            #connect funtion to button
+            btn.clicked.connect(self.asset_clicked)
+            self.set_icons_size(self.icon_size_slider.value())
+            self.status_bar.setText("total assets : %s"%(len(asset_in_catagory)))
+
+
+        self.setLayout(self.main_layout)
+
+
+
+    def set_cagagories(self):
+        if len(os.listdir(json_folder))==0: #check and download json files
             self.write_json_to_local()
 
         if self.asset_type.currentIndex() == 0:
-            json_foder_index = json_folder + "hdris.json"
-            self.get_cagagories("hdris")
+            asset_type = "hdris"
         elif self.asset_type.currentIndex() == 1:
-            json_foder_index = json_folder + "models.json"
-            self.get_cagagories("models")
+            asset_type = "models"
         elif self.asset_type.currentIndex() == 2:
-            json_foder_index = json_folder + "textures.json"
-            self.get_cagagories("textures")
+            asset_type = "textures"
         else:
-            json_foder_index = "https://api.polyhaven.com/assets?t=hdris&c=studio"
+            asset_type = "hdris"
 
-        self.clear_layout(self.assets_view)
-        self.set_icons(json_foder_index)
-        
-    def get_cagagories(self, asset_type="hdris"):
         catagory_json = json_folder + asset_type + "_catagories.json"
         self.asset_catagories.clear()
-        with open(catagory_json, "r") as read_content:
+        with open(catagory_json, "r") as read_content: #read catagory form json
             d = json.load(read_content)
             catagory = d.keys()
             for i, key in enumerate(catagory):
                 self.asset_catagories.insertItem(i, key)
-        self.asset_catagories.setCurrentIndex(1)
+
+        self.asset_catagories.setCurrentIndex(1) #set catagory to second option
 
 
     def write_json_to_local(self):
         assets_types = ["hdris", "textures", "models"]
         for type in assets_types:
+            self.status_bar.setText("downloading %s json files"%(type))
             assets_json = main_api + "assets?t=%s" % (type)
             categories_json =  main_api + "categories/%s" %(type)
 
@@ -181,57 +259,9 @@ class MainAssetBrowserUI(QtWidgets.QWidget):
     def clear_layout(self, layout):
         while layout.count():
             child = layout.takeAt(0)
+            self.status_bar.setText("clearing %s from contant area."%(child.widget().objectName()))
             if child.widget():
                 child.widget().deleteLater()
-
-    def set_icons(self, json_foder_index, size=200):
-        catagory = self.asset_catagories.currentText()
-        with open(json_foder_index, "r") as read_content:
-            d = json.load(read_content)
-            for key in d.keys():
-                if catagory in (d[key]["categories"]):
-                    # print(key)
-
-        # with open(json_foder_index, "r") as read_content: #ping the local json
-        #     self.data = json.load(read_content)
-        #     # print(d.keys())
-
-        # for key in self.data.keys():
-                    btn = QtWidgets.QToolButton()
-                    btn.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)  
-                    btn.setFixedSize(QtCore.QSize(size, size))
-                    btn.setIconSize(QtCore.QSize(size, size))
-                    btn.setText(key.replace("_", " ").title())
-                    btn.setObjectName(key)
-                    btn.setStyleSheet("QToolButton{font-family: Roboto}")
-
-
-                    thumb_name = (("{0}.png".format(btn.objectName())))
-                    if not thumb_name in os.listdir(thumbnail_folder):
-
-                        url = thumb_url + key + ".png?height=" + str(500)
-                        req = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
-                        
-                        download = IconDownloader(btn, req)
-                        download.start_fetch(self.download_queue)
-                        self.assets_view.addWidget(btn)
-                        self.assets_view.setSpacing(4)
-                        self.setLayout(self.main_layout)
-
-                    #get icons form local thumbnail folder
-                    get_local_thumb = thumbnail_folder + thumb_name
-                    icon = QtGui.QIcon()
-                    icon.addPixmap(get_local_thumb)
-                    btn.setIcon(icon)
-
-                    #icon add to content area
-                    self.assets_view.addWidget(btn)
-                    self.setLayout(self.main_layout)
-
-                    #connect funtion to button
-                    btn.clicked.connect(self.asset_clicked)
-                    self.set_icons_size(200)
-
 
     def check_asset_download_status(self):
         icons = self.contentArea.findChildren(QtWidgets.QToolButton)
@@ -247,10 +277,10 @@ class MainAssetBrowserUI(QtWidgets.QWidget):
         icons = self.contentArea.findChildren(QtWidgets.QToolButton)
         font_size = int(5*(int(size)*0.01))
         border_size = 3
-
+        icon_size_ratio = 0.8
         for icon in icons:
             icon.setFixedSize(QtCore.QSize(size, size))
-            icon.setIconSize(QtCore.QSize(size, size))
+            icon.setIconSize(QtCore.QSize(size * icon_size_ratio, size * icon_size_ratio))
             icon.setStyleSheet("QToolButton{font-size: %spt}"%(str(font_size)))
 
             local_asset_name = (("{0}_{1}.{2}".format(icon.objectName(), self.tex_res.currentText(), self.asset_format.currentText())))
@@ -266,12 +296,10 @@ class MainAssetBrowserUI(QtWidgets.QWidget):
         tex_res = self.tex_res.currentText()
         asset_fomat = self.asset_format.currentText()
         path_to_check = "{0}{1}_{2}.{3}".format(download_folder, name, tex_res, asset_fomat)
-        # print(tex_res)
         if not os.path.exists(path_to_check):
             self.progress_bar.setProperty("visible", True)
             self.progress_bar.setValue(0)
             asset_json = requests.get(asset_url + name).json()
-            # print(asset_json)
             self.url = asset_json["hdri"][tex_res][asset_fomat]["url"]
             self.file_size = asset_json["hdri"][tex_res][asset_fomat]["size"]
 
